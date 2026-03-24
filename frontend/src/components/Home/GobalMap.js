9bb0f5c4610fabd64ac88f571bd3b074c97a45b7
akhil s <akhilss235@gmail.com>
2026-03-24 16:26:10 +0530

    glitch changed

--- frontend/src/components/Home/GobalMap.js
+++ frontend/src/components/Home/GobalMap.js
@@ -1315,6 +1315,7 @@ export default function GobalMap() {
 
   useEffect(() => {
     if (!isKerala) return;
+    setIndiaZoomComplete(false);
     setActiveIndiaClass(null);
     setHoverIndiaClass(null);
     activeIndiaClassRef.current = null;
