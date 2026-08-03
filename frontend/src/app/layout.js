2e315051282e77d900096182a52863b4fee3643f
akhilsflumenx <akhilsflumencopia@gmail.com>
2026-08-03 11:15:48 +0530

    ui

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -7,18 +7,23 @@ import StartupLoader from "@/components/StartupLoader";
 export const metadata = {
   title: "State Soil Museum",
   description: "State Soil Museum",
+  icons: {
+    icon: "/images/logo.png",
+    shortcut: "/images/logo.png",
+    apple: "/images/logo.png",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-            <link
-  rel="stylesheet"
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-/>
-
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        />
+      </head>
       <body>
-
         <StartupLoader />
         <InteractionSound />
         <HeroBackgroundMusic />
